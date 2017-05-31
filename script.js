02f65729777eae92e8b6001a1174ab45f7728ea3
Margo Schaedel <margoschaedel@gmail.com>
2017-05-31 16:37:49 -0600

    Refactor upvote and downvote functionality

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-//***********************************************************
-//  object
-//***********************************************************
+loadPage();
 
 function Task(title, body)  {
   this.title = title;
@@ -10,35 +8,15 @@ function Task(title, body)  {
   this.isComplete = false;
 }
 
-//************************************************************
-//  event listeners
-//************************************************************
 
-loadPage();
-
-$('.show-completed').on('click', showCompletedTasks)
-
-$('.card-container').on('click', '.completed-task', completeTask);
-
-$('.search-input').on('input', filterList);
-
-$('.input-container').on('input', enableSaveButton);
-
-$('.save-button').on('click', saveNewItem);
-
-$(window).on('keyup', enterSave)
-
-$('.card-container').on('input keydown', '.task-input', enterEdit)
-
-$('.card-container').on('click', '.delete-button', deleteItem);
-
-$('.card-container').on('keyup', '.task-title', editTitle);
-
-$('.card-container').on('keyup', '.task-body', editTask);
-
-$('.card-container').on('click', '.arrow-up', changeUpvoteImportance);
-
-$('.card-container').on('click', '.arrow-down', changeDownvoteImportance);
+$('.card-container')
+  .on('input keydown', '.task-input', enterEdit)
+  .on('click', '.delete-button', deleteItem)
+  .on('keyup', '.task-title', editTitle)
+  .on('keyup', '.task-body', editTask)
+  .on('click', '.arrow-up', changeUpvote)
+  .on('click', '.arrow-down', changeDownvote)
+  .on('click', '.completed-task', completeTask);
 
 $('.critical-importance').on('click', filterImportance);
 $('.high-importance').on('click', filterImportance);
@@ -46,34 +24,51 @@ $('.normal-importance').on('click', filterImportance);
 $('.low-importance').on('click', filterImportance);
 $('.no-importance').on('click', filterImportance);
 
-//********************************************************************************
-//   functions
-//*********************************************************************************
+$('.input-container').on('input', enableSaveButton);
+$('.save-button').on('click', saveNewItem);
+$('.search-input').on('input', filterList);
+$('.show-completed').on('click', showCompletedTasks)
+$('.show-more').on('click', completedList);
+$(window).on('keyup', enterSave)
 
-function prepend(task)  {
-  if (task.isComplete) {
-    var completeTask = 'complete';
-  }
 
-  $('.card-container').prepend(`
-    <article class='task-card ${completeTask}'id=${task.id}>
-      <input class='task-title task-input' value='${task.title}'>
-      <button class='delete-button'></button>
-      <textarea cols='30' rows='10' class='task-body task-input' type='text' value=''>${task.body}</textarea>
-      <section class='button-container'>
-        <button class='arrow-up'></button>
-        <button class='arrow-down'></button>
-        <p class='importance'>Importance:&nbsp</p>
-        <p class='importance-value'> ${task.importance}</p>
-        <button class='completed-task'>Completed Task</button>
-      </section>
-      <hr />
-    </article>
-    `)
+function changeDownvote() {
+  var importanceArray = ['None', 'Low', 'Normal', 'High', 'Critical']
+  var id = $(this).parent().parent().prop('id');
+  var parsedTask = JSON.parse(localStorage.getItem(id));
+  var currentImportance = $(this).siblings('.importance-value').text();
+  var index = importanceArray.indexOf(currentImportance);
+  $(this).siblings('.importance-value').text(importanceArray[index - 1]);
+  parsedTask.importance = $(this).siblings('.importance-value').text();
+  localStorage.setItem(id, JSON.stringify(parsedTask));
 }
 
-function showTenTasks() {
-  $('.task-card').slice(10).hide();
+function changeUpvote() {
+  var importanceArray = ['None', 'Low', 'Normal', 'High', 'Critical']
+  var id = $(this).parent().parent().prop('id');
+  var parsedTask = JSON.parse(localStorage.getItem(id));
+  var currentImportance = $(this).siblings('.importance-value').text();
+  var index = importanceArray.indexOf(currentImportance);
+  $(this).siblings('.importance-value').text(importanceArray[index + 1]);
+  parsedTask.importance = $(this).siblings('.importance-value').text();
+  localStorage.setItem(id, JSON.stringify(parsedTask));
+}
+
+function clearInputFields() {
+  $('.input-title').val('');
+  $('.input-body').val('');
+  $('.input-title').focus();
+}
+
+function completedList() {
+  var filterCompleteList = [];
+  var fullList = getAllFromLocalStorage();
+  filterCompleteList = fullList.filter(function(item) {
+    return item.isComplete === false;
+  })
+  if (filterCompleteList.length > 0) {
+    displaySearchResults(filterCompleteList);
+  }
 }
 
 function completeTask(complete) {
@@ -107,22 +102,20 @@ function editTask() {
 	localStorage.setItem(id, JSON.stringify(parsedTask))
 }
 
-// enable save button on return
 function enableSaveButton13()  {
   var title = $('.input-title').val();
   var body = $('.input-body').val();
-  var task = new Task(title, body);// create a new Task object and pass through the captured input and body values
+  var task = new Task(title, body);
     if (title === "" || body === "") {
       $('.save-button').prop('disabled', true)
   } else {$('.save-button').prop('disabled', false)
-  prepend(task); // add the new task card to the card area
-  clearInputFields();  // clear the user input and body values
-  sendToStorage(task); // set the item and stringify to local storage
+  prepend(task);
+  clearInputFields();
+  sendToStorage(task);
   disableSaveButton();
   }
 }
 
-// enable save button
 function enableSaveButton()  {
   var taskTitle = $('.input-title').val();
   var taskBody = $('.input-body').val();
@@ -132,7 +125,10 @@ function enableSaveButton()  {
   }
 }
 
-// edit title or body upon pressing enter key
+function enableShowCompleteBtn() {
+  $('.show-completed').prop('disabled', false)
+}
+
 function enterEdit(e) {
   if (e.keyCode === 13) {
     e.preventDefault();
@@ -141,51 +137,31 @@ function enterEdit(e) {
   }
 }
 
-// save task upon pressing enter key
 function enterSave(e) {
   if(e.keyCode === 13 && ($('.input-title').val() !== '') && ($('.input-body').val() !== '')){
     enableSaveButton13();
   }
 }
 
-// disable save button
 function disableSaveButton() {
   $('.save-button').prop('disabled', true)
 }
 
 function disableShowCompleteBtn() {
   $('.show-completed').prop('disabled', true)
-  // if ($('.show-completed').attr('disabled', true))
 }
 
-function enableShowCompleteBtn() {
-  $('.show-completed').prop('disabled', false)
+function displayCompletedTaskResults(taskResults) {
+  taskResults.forEach(function(item) {
+    prepend(item);
+  });
 }
 
-// clear input fields
-function clearInputFields() { //clear the title and body input fields
-  $('.input-title').val('');
-  $('.input-body').val('');
-  $('.input-title').focus();
-}
-
-// send task to local storage
-function sendToStorage(task)  {
-  localStorage.setItem(task.id, JSON.stringify(task));
-}
-
-// get an task card from storage by id
-function getFromStorage(id) {
-	var parsedTask = JSON.parse(localStorage.getItem(id))
-	return parsedTask;
-}
-
-function getAllFromLocalStorage(){
-  var allItems = [];
-  for (var i = 0; i < localStorage.length; i++) {
-    allItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-  }
-  return allItems;
+function displaySearchResults(searchResults) {
+  $('.card-container').empty();
+  searchResults.forEach(function(item) {
+    prepend(item);
+  });
 }
 
 function filterImportance() {
@@ -207,22 +183,67 @@ function filterList(){
   var searchText = $('.search-input').val().toUpperCase();
   var fullList = getAllFromLocalStorage();
   filteredList = fullList.filter(function(item){
-   return item.title.toUpperCase().includes(searchText) || item.body.toUpperCase().includes(searchText)
+    return item.title.toUpperCase().includes(searchText) || item.body.toUpperCase().includes(searchText)
   })
   if (filteredList.length > 0) {
     displaySearchResults(filteredList);
   }
 }
 
-function completedList() {
-  var filterCompleteList = [];
-  var fullList = getAllFromLocalStorage();
-  filterCompleteList = fullList.filter(function(item) {
-    return item.isComplete === false;
-  })
-  if (filterCompleteList.length > 0) {
-    displaySearchResults(filterCompleteList);
+function getAllFromLocalStorage(){
+  var allItems = [];
+  for (var i = 0; i < localStorage.length; i++) {
+    allItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
   }
+  return allItems;
+}
+
+function getFromStorage(id) {
+	var parsedTask = JSON.parse(localStorage.getItem(id))
+	return parsedTask;
+}
+
+function loadPage() {
+  for (var i = 0; i < localStorage.length; i++) {
+    prepend(JSON.parse(localStorage.getItem(localStorage.key(i))));
+  }
+  completedList();
+  showTenTasks();
+}
+
+function prepend(task)  {
+  if (task.isComplete) {
+    var completeTask = 'complete';
+  }
+  $('.card-container').prepend(`
+    <article class='task-card ${completeTask}'id=${task.id}>
+    <input class='task-title task-input' value='${task.title}'>
+    <button class='delete-button'></button>
+    <textarea cols='30' rows='10' class='task-body task-input' type='text' value=''>${task.body}</textarea>
+    <section class='button-container'>
+    <button class='arrow-up'></button>
+    <button class='arrow-down'></button>
+    <p class='importance'>Importance:&nbsp</p>
+    <p class='importance-value'>${task.importance}</p>
+    <button class='completed-task'>Completed Task</button>
+    </section>
+    <hr />
+    </article>
+    `)
+  }
+
+  function saveNewItem() {
+    var title = $('.input-title').val();
+    var body = $('.input-body').val();
+    var task = new Task(title, body);
+    prepend(task);
+    clearInputFields();
+    sendToStorage(task);
+    disableSaveButton();
+  }
+
+function sendToStorage(task)  {
+  localStorage.setItem(task.id, JSON.stringify(task));
 }
 
 function showCompletedTasks() {
@@ -233,108 +254,11 @@ function showCompletedTasks() {
   })
   if (filterCompleteList.length > 0) {
       loadPage();
-      AllDisplaySearchResults(filterCompleteList);
+      displayCompletedTaskResults(filterCompleteList);
       disableShowCompleteBtn();
   }
 }
 
-function AllDisplaySearchResults(searchResults) {
-  $('.card-container').prepend();
-  searchResults.forEach(function(item) {
-    prepend(item);
-  })
+function showTenTasks() {
+  $('.task-card').slice(10).hide();
 }
-
-function displaySearchResults(searchResults) {
-  $('.card-container').empty();
-  searchResults.forEach(function(item) {
-    prepend(item);
-  })
-}
-
-function loadPage() {
-  for (var i = 0; i < localStorage.length; i++) { // run for loop over entire length of local storage array
-		prepend(JSON.parse(localStorage.getItem(localStorage.key(i))));
-  }
-  completedList();
-  showTenTasks();
-}
-
-function saveNewItem() {
-  var title = $('.input-title').val();// capture input value
-  var body = $('.input-body').val();// capture body value
-  var task = new Task(title, body);// create a new Task object and pass thru the captured input and body values
-  prepend(task); // add the new task card to the card area
-  clearInputFields();  // clear the user input and body values
-  sendToStorage(task);
-  disableSaveButton();
-  loadPage();
-}
-
-function changeUpvoteImportance() {
-  var id = $(this).parent().parent().prop('id');//get the unique id of this task card
-  var parsedTask = JSON.parse(localStorage.getItem(id));//get this task card from storage and parse it
-  var currentImportance = parsedTask.importance;//get the current importance of this task card
-  // adjust the importance level based on the current importance
-  if(currentImportance === 'None') {
-    parsedTask.importance = 'Low'// change the object task card's importance
-    $(this).siblings('.importance-value').text('Low');// change the importance of the task card on the DOM
-  } else if(currentImportance === 'Low') {
-    parsedTask.importance = 'Normal'// change the object task card's importance
-    $(this).siblings('.importance-value').text('Normal');// change the importance of the task card on the DOM
-  } else if(currentImportance === 'Normal') {
-    parsedTask.importance = 'High' // change the object task card's importance
-    $(this).siblings('.importance-value').text('High'); // change the importance of the task card on the DOM
-  } else if(currentImportance === 'High') {
-    parsedTask.importance = 'Critical' // change the object task card's importance
-    $(this).siblings('.importance-value').text('Critical'); // change the importance of the task card on the DOM
-  }
-  localStorage.setItem(id, JSON.stringify(parsedTask));// return the updated object task card to local storage
-}
-
-function changeDownvoteImportance() {
-  var id = $(this).parent().parent().prop('id');//get the unique id of this task card
-  var parsedTask = JSON.parse(localStorage.getItem(id));//get this task card from storage and parse it
-  var currentImportance = parsedTask.importance;//get the current importance of this task card
-  // adjust the importance level based on the current importance
-  if(currentImportance === 'Critical') {
-    parsedTask.importance = 'High'// change the object task card's importance
-    $(this).siblings('.importance-value').text('High');// change the importance of the task card on the DOM
-  } else if(currentImportance === 'High') {
-    parsedTask.importance = 'Normal'// change the object task card's importance
-    $(this).siblings('.importance-value').text('Normal');// change the importance of the task card on the DOM
-  } else if(currentImportance === 'Normal') {
-    parsedTask.importance = 'Low' // change the object task card's importance
-    $(this).siblings('.importance-value').text('Low'); // change the importance of the task card on the DOM
-  } else if(currentImportance === 'Low') {
-    parsedTask.importance = 'None' // change the object task card's importance
-    $(this).siblings('.importance-value').text('None'); // change the importance of the task card on the DOM
-  }
-  localStorage.setItem(id, JSON.stringify(parsedTask));// return the updated object task card to local storage
-}
-
-// function switchUpvote(currentImportance) {
-//   console.log('parsedTask current importance: ' + currentImportance);
-//   switch (currentImportance) {
-//     case 'None':
-//       currentImportance = 'Low';
-//       $(this).find('.importance-value').text('Low');
-//       break;
-//     case 'Low':
-//       currentImportance = 'Normal';
-//       $(this).find('.importance-value').text('Normal');
-//       break;
-//     case 'Normal':
-//       currentImportance = 'High';
-//       $(this).find('.importance-value').text('High');
-//       break;
-//     case 'High':
-//       currentImportance = 'Critical';
-//       $(this).find('.importance-value').text('Critical');
-//       break;
-//     default:
-//       console.log(($(this).find('.importance-value')));
-//       console.log('switch is not working');
-//       break;
-//   }
-// }
